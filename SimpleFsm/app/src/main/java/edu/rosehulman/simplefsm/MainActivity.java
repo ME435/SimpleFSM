aa5d9bac6ea44b83c469ecfed1a7fb6e5d559bf9
David Fisher <fisherds@rose-hulman.edu>
2015-04-09 23:44:08 -0400

    Solution code

--- SimpleFsm/app/src/main/java/edu/rosehulman/simplefsm/MainActivity.java
+++ SimpleFsm/app/src/main/java/edu/rosehulman/simplefsm/MainActivity.java
@@ -2,36 +2,140 @@ package edu.rosehulman.simplefsm;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends Activity {
 
-  private TextView mCurrentStateTextView, mStateTimeTextView;
-  
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    
-    mCurrentStateTextView = (TextView) findViewById(R.id.current_state_textview);
-    mStateTimeTextView = (TextView) findViewById(R.id.state_time_textview);
-  }
+    public enum State {
+        READY, RUNNING_A_SCRIPT, SEEKING_HOME, SUCCESS,
+    }
+    private State mState = State.READY;
+
+    private TextView mCurrentStateTextView, mStateTimeTextView;
+    private Handler mCommandHandler = new Handler();
+
+    private long mStateStartTime = 0;
+    protected Timer mTimer;
+    public static final int LOOP_INTERVAL_MS = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mCurrentStateTextView = (TextView) findViewById(R.id.current_state_textview);
+        mStateTimeTextView = (TextView) findViewById(R.id.state_time_textview);
+        setState(State.READY);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mTimer = new Timer();
+        mTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        loop();
+                    }
+                });
+            }
+        }, 0, LOOP_INTERVAL_MS);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mTimer.cancel();
+        mTimer = null;
+    }
 
 
-  public void handleGo(View view) {
-    Toast.makeText(this, "You pressed Go!", Toast.LENGTH_SHORT).show();
-    // TODO: If in the ready state, set the state to Running a script.
-  }
-  
-  public void handleReset(View view) {
-    Toast.makeText(this, "You pressed Reset", Toast.LENGTH_SHORT).show();
-    // TODO: Set the state to Ready.
-  }
-  
-  public void handleHitTarget(View view) {
-    Toast.makeText(this, "You pressed Hit Target", Toast.LENGTH_SHORT).show();
-    // TODO: If in the seeking home state, set the state to Success.
-  }
+    public void loop() {
+        mStateTimeTextView.setText("" + getStateTimeMs() / 1000);
+
+        switch (mState) {
+            case SEEKING_HOME:
+                if (getStateTimeMs() > 6000) {
+                    setState(State.READY);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void setState(State newState) {
+        mStateStartTime = System.currentTimeMillis();
+        mCurrentStateTextView.setText(newState.name());
+
+        switch (newState) {
+            case READY:
+                break;
+            case RUNNING_A_SCRIPT:
+                runScript();
+                break;
+            case SEEKING_HOME:
+                // Do nothing here.
+                break;
+            case SUCCESS:
+                break;
+        }
+        mState = newState;
+    }
+
+
+    public void handleGo(View view) {
+        if (mState == State.READY) {
+            setState(State.RUNNING_A_SCRIPT);
+        }
+    }
+
+    public void handleReset(View view) {
+        if (mState == State.SEEKING_HOME || mState == State.SUCCESS) {
+            setState(State.READY);
+        }
+    }
+
+    public void handleHitTarget(View view) {
+        if (mState == State.SEEKING_HOME) {
+            setState(State.SUCCESS);
+        }
+    }
+
+    private void runScript() {
+        Toast.makeText(this, "Run script step 0", Toast.LENGTH_SHORT).show();
+        mCommandHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, "Run script step 1", Toast.LENGTH_SHORT).show();
+            }
+        }, 2000);
+        mCommandHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, "Run script step 2", Toast.LENGTH_SHORT).show();
+            }
+        }, 4000);
+        mCommandHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (mState == State.RUNNING_A_SCRIPT) {
+                    setState(State.SEEKING_HOME);
+                }
+            }
+        }, 6000);
+    }
+
+
+    protected long getStateTimeMs() {
+        return System.currentTimeMillis() - mStateStartTime;
+    }
 }
